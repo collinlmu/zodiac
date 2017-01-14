93f9c29d2ab670bde92a27ea3bceda007cebac17
Collin <collin.ehret@gmail.com>
2017-01-14 15:45:01 -0800

    javascript additions

--- js/javascript.js
+++ js/javascript.js
@@ -2,85 +2,79 @@ var zodiacs = [
   {
     name: 'Aries',
     dates: '21 March - 20 April',
-    imgage: '',
     characteristics:'',
-    compatability
+    compatability: ''
   },
   {
     name: 'Taurus',
     dates: '21 April - 21 May',
-    imgage: '',
     characteristics:'',
-    compatability
+    compatability: ''
   },
   {
     name: 'Gemini',
     dates: '22 May - 21 June',
-    imgage: '',
     characteristics:'',
-    compatability
+    compatability: ''
   },
   {
     name: 'Cancer',
     dates: '22 June - 22 July',
-    imgage: '',
     characteristics:'',
-    compatability
+    compatability: ''
   },
   {
     name: 'Leo',
     dates: '23 July - 22 August',
-    imgage: '',
     characteristics:'',
-    compatability
+    compatability: ''
   },
   {
     name: 'Virgo',
     dates: '23 August - 23 September',
-    imgage: '',
     characteristics:'',
-    compatability
+    compatability: ''
   },
   {
     name: 'Libra',
     dates: '23 September - 23 October',
-    imgage: '',
     characteristics:'',
-    compatability
+    compatability: ''
   },
   {
     name: 'Scorpipo',
     dates: '24 October - 22 November',
-    imgage: '',
     characteristics:'',
-    compatability
+    compatability: ''
   },
   {
     name: 'Sagittarius',
     dates: '23 November - 21 December',
-    imgage: '',
     characteristics:'',
-    compatability
+    compatability: ''
   },
   {
     name: 'Capricorn',
     dates: '22 December - 20 January',
-    imgage: '',
     characteristics:'',
-    compatability
+    compatability: ''
   },
   {
     name: 'Aquarius',
     dates: '21 January - 19 February',
-    imgage: '',
     characteristics:'',
-    compatability
+    compatability: ''
   },
   {
     name: 'Pisces',
     dates: '20 February - 20 March',
-    imgage: '',
     characteristics:'',
-    compatability
+    compatability: ''
   }
-]
+];
+
+for (var i = 0; i < zodiacs.length; i++) {
+  if (zodiacs[i]['name'] === 'ZodiacName') {
+    console.log('Name: ', zodiacs[i]['name'])
+  }
+}
