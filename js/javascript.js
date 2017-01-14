165086ac5aa23e6018d26a66ade646e2e04b2d7f
Collin <collin.ehret@gmail.com>
2017-01-14 14:52:36 -0800

    First Commit

---
+++ js/javascript.js
@@ -0,0 +1,86 @@
+var zodiacs = [
+  {
+    name: 'Aries',
+    dates: '21 March - 20 April',
+    imgage: '',
+    characteristics:'',
+    compatability
+  },
+  {
+    name: 'Taurus',
+    dates: '21 April - 21 May',
+    imgage: '',
+    characteristics:'',
+    compatability
+  },
+  {
+    name: 'Gemini',
+    dates: '22 May - 21 June',
+    imgage: '',
+    characteristics:'',
+    compatability
+  },
+  {
+    name: 'Cancer',
+    dates: '22 June - 22 July',
+    imgage: '',
+    characteristics:'',
+    compatability
+  },
+  {
+    name: 'Leo',
+    dates: '23 July - 22 August',
+    imgage: '',
+    characteristics:'',
+    compatability
+  },
+  {
+    name: 'Virgo',
+    dates: '23 August - 23 September',
+    imgage: '',
+    characteristics:'',
+    compatability
+  },
+  {
+    name: 'Libra',
+    dates: '23 September - 23 October',
+    imgage: '',
+    characteristics:'',
+    compatability
+  },
+  {
+    name: 'Scorpipo',
+    dates: '24 October - 22 November',
+    imgage: '',
+    characteristics:'',
+    compatability
+  },
+  {
+    name: 'Sagittarius',
+    dates: '23 November - 21 December',
+    imgage: '',
+    characteristics:'',
+    compatability
+  },
+  {
+    name: 'Capricorn',
+    dates: '22 December - 20 January',
+    imgage: '',
+    characteristics:'',
+    compatability
+  },
+  {
+    name: 'Aquarius',
+    dates: '21 January - 19 February',
+    imgage: '',
+    characteristics:'',
+    compatability
+  },
+  {
+    name: 'Pisces',
+    dates: '20 February - 20 March',
+    imgage: '',
+    characteristics:'',
+    compatability
+  }
+]
